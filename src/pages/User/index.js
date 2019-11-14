f6716f6c7eb4ab67455b9fe64c32c73b8dd8ad7a
burn-c <chopcsom@gmail.com>
2019-11-14 20:07:59 -0300

    Feature: ScrollInfinity

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -31,33 +31,45 @@ export default class User extends Component {
 
   state = {
     load: true,
+    page: 1,
     stars: [],
   };
 
+  // Executa assim que a pagina abre
   async componentDidMount() {
+    this.load();
+  }
+
+  load = async (page = 1) => {
+    const { stars } = this.state;
     const { navigation } = this.props;
     const user = navigation.getParam('user');
 
-    const response = await api.get(`/users/${user.login}/starred`);
+    const response = await api.get(`/users/${user.login}/starred`, {
+      params: { page },
+    });
+
     this.setState({
-      stars: response.data,
+      stars: page >= 2 ? [...stars, ...response.data] : response.data,
+      page,
       load: false,
     });
-  }
+  };
+
+  loadMore = () => {
+    const { page } = this.state;
+    const nextPage = page + 1;
+    this.load(nextPage);
+    this.setState({
+      load: true,
+    });
+  };
 
   render() {
     const { navigation } = this.props;
     const { stars, load } = this.state;
     const user = navigation.getParam('user');
 
-    if (load) {
-      return (
-        <Load>
-          <ActivityIndicator size="large" color="#7159c1" />
-          <LoadText>Carregando...</LoadText>
-        </Load>
-      );
-    }
     return (
       <Container>
         <Header>
@@ -65,19 +77,29 @@ export default class User extends Component {
           <Name>{user.name}</Name>
           <Bio>{user.bio}</Bio>
         </Header>
-        <Stars
-          data={stars}
-          keyExtractor={star => String(star.id)}
-          renderItem={({ item }) => (
-            <Starred>
-              <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
-              <Info>
-                <Title>{item.name}</Title>
-                <Author>{item.owner.login}</Author>
-              </Info>
-            </Starred>
-          )}
-        />
+
+        {load ? (
+          <Load>
+            <ActivityIndicator size="large" color="#7159c1" />
+            <LoadText>Carregando...</LoadText>
+          </Load>
+        ) : (
+          <Stars
+            onEndReachedThreshold={0.2} // 20% do final da lista
+            onEndReached={this.loadMore} // Executa a função quando chega no 20% final
+            data={stars}
+            keyExtractor={star => String(star.id)}
+            renderItem={({ item }) => (
+              <Starred>
+                <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+                <Info>
+                  <Title>{item.name}</Title>
+                  <Author>{item.owner.login}</Author>
+                </Info>
+              </Starred>
+            )}
+          />
+        )}
       </Container>
     );
   }
